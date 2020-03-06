9614fa4bafeb15e51ddfad027ded9b8f6cbecdc1
Walefe <walefedev@gmail.com>
2020-03-06 08:50:42 -0300

    Chore: adds validation to form fields.

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import * as Yup from 'yup';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('e-mail inválido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="FastFeet" />
 
-      <form>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <strong>seu e-mail</strong>
-        <input type="email" placeholder="exemplo@email.com" />
+        <Input name="email" type="email" placeholder="exemplo@email.com" />
         <strong>sua senha</strong>
-        <input type="password" placeholder="*************" />
-      </form>
-
-      <button type="submit">Entrar no sistema</button>
+        <Input name="password" type="password" placeholder="*************" />
+        <button type="submit">Entrar no sistema</button>
+      </Form>
     </>
   );
 }
