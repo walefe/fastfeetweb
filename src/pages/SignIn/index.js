ab2f76e104cb7b7e9e027677749490de06cee084
Walefe <walefedev@gmail.com>
2020-03-06 08:23:52 -0300

    Chore: create auth layout, SignIn page and styles for layout.

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import logo from '~/assets/logo.png';
+
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="FastFeet" />
+
+      <form>
+        <strong>seu e-mail</strong>
+        <input type="email" placeholder="exemplo@email.com" />
+        <strong>sua senha</strong>
+        <input type="password" placeholder="*************" />
+      </form>
+
+      <button type="submit">Entrar no sistema</button>
+    </>
+  );
 }
